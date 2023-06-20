79b7266dc5af8bd4a1d235a77b2c433c4ccc2388
Shivanshu Dubey <sdubey@Shivanshus-MacBook-Pro.local>
2023-06-21 00:43:50 +0530

    commit from terraform workflow

---
+++ apps/customer-registration-service.py
@@ -0,0 +1,34 @@
+from orms import *
+from faker import Faker
+from random import choice, uniform
+
+def main():
+    db = Session()
+    print("Starting to create dataset for {} times".format(os.environ.get("ITERATION_COUNT")))
+    try:
+        for i, x in enumerate(range(0, int(os.environ.get("ITERATION_COUNT")))):
+        # for x in range(0, 10):
+            print("Creating dataset for {} time".format(i))
+            fake = Faker()
+            name = fake.name
+            customer_registration = CustomerRegistration(
+                FIRST_NAME = fake.first_name,
+                LAST_NAME = fake.last_name,
+                EMAIL = fake.email,
+                GENDER = choice(["MALE", "FEMALE", "OTHER"]),
+                INCOME = uniform(1000, 1000000),
+                FICO = uniform(100,800)
+            )
+            print("{0}:{1}".format(i, customer_registration.__dict__))
+            db.add(customer_registration)
+            db.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        db.close()
+
+if __name__=="__main__":
+    print("Starting batch job to produce dataset")
+    main()
+    print("Gracefully exitiing...")
+
---
+++ apps/orms.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, Text, DateTime, func
+import datetime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+import os
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://{0}:{1}@{2}/{3}".format(os.environ.get("DB_USER"), os.environ.get("DB_PASS"), os.environ.get("DB_URI"), os.environ.get("DB_NAME"))
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}
+)
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class CustomerRegistration(Base):
+    __tablename__ = "customer_registration"
+
+    CUSTOMER_ID = Column(Integer, primary_key=True, autoincrement=True)
+    FIRST_NAME = Column(String, unique=False, index=False, nullable=False)
+    LAST_NAME = Column(String, unique=False, index=False, nullable=False)
+    EMAIL = Column(String, default="", unique=False, index=True)
+    GENDER = Column(String, default="MALE", unique=False, index=True)
+    INCOME = Column(Integer, default=0, unique=False, index=True)
+    FICO = Column(Integer, default=0, unique=False, index=True)
+    CREATED_AT = Column(DateTime(timezone=True), nullable=False,
+                  server_default=func.now())
+
+    CUSTOMER_ACTIVITY = relationship("CustomerActivity", back_populates="CUSTOMER_REGISTRATION")
+
+class CustomerActivity(Base):
+    __tablename__="customer_activity"
+
+    ACTIVITY_ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    IP_ADDRESS = Column(String, unique=False, nullable=False)
+    ACTIVITY_TYPE = Column(String, unique=False, nullable=False)
+    PROPENSITY_TO_BUY = Column(Float, unique=False, nullable=False, default=0.0)
+    CUSTOMER_ID = Column(Integer, ForeignKey("customer_registration.CUSTOMER_ID"))
+
+    CUSTOMER_REGISTRATION = relationship("CustomerRegistration", back_populates="CUSTOMER_ACTIVITY")

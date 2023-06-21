09d364bf0b26a34c23e67879130d0da41eaed341
Shivanshu Dubey <sdubey@Shivanshus-MacBook-Pro.local>
2023-06-21 19:24:16 +0530

    commit from terraform workflow

---
+++ apps/activity-offer-service.py
@@ -0,0 +1,41 @@
+import os
+from google.cloud.pubsub_v1 import PublisherClient
+import json
+from typing import List
+from random import randint
+from faker import Faker
+
+def get_data(dataset:List[dict]):
+        fake = Faker()
+        data = dataset[randint(0, len(dataset)-1)]
+        data["offer_id"] = fake.lexify()
+        data["offer_name"] = fake.bs()
+
+        return data
+
+def generator():
+    
+    with open('{}/activity-offers.json'.format(os.path.curdir), 'r') as f:
+        dataset = json.load(f)
+
+    publisher = PublisherClient()
+
+    topic_name = 'projects/{project_id}/topics/{topic}'.format(
+        project_id=os.environ.get('GCP_PROJECT'),
+        topic=os.environ.get('GCP_PUB_SUB_TOPIC'),  # Set this to something appropriate.
+    )
+    
+    try:
+         for i, x in enumerate(range(0, int(os.environ.get("ITERATION_COUNT")))): 
+            data = json.dumps(get_data(dataset))
+            future = publisher.publish(topic_name, data.encode(), spam='eggs')
+            print(future.result())
+    except Exception as e:
+        print(e)
+    finally:
+         publisher.stop()
+
+if __name__=="__main__":
+    print("Starting batch job to produce dataset")
+    generator()
+    print("Gracefully exitiing...")
